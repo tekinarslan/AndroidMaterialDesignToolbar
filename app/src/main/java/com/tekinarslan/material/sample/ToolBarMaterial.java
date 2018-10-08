9ab65178dde56314dce27cfcf68db7a78b06f372
atul anand <37778673+atul161@users.noreply.github.com>
2018-10-08 16:43:40 +0530

    Update and rename ToolBar.java to ToolBarMaterial.java

---
+++ app/src/main/java/com/tekinarslan/material/sample/ToolBarMaterial.java
@@ -0,0 +1,68 @@
+package com.tekinarslan.material.sample;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+
+public class ToolbarMaterial extends AppCompactActivity
+    {
+       
+       public void onCreate(Bundle bundle)
+         {
+            super.onCreate(bundle);
+            setContentView(R.layout.ToolbarMaterial);
+         }
+         
+         
+         public void Item1(View view)
+             {
+               Button button1 =findViewById(R.id.button1);
+               button1.setOnClickListener(new OnClickListener()
+                     {
+                         public void onClick(View view)
+                              {
+                                 
+                                   Toast.makeText(this,"Hey you Have done this!!",Toast.LENGTH_LONG).show();
+                              }
+                      });
+               }
+               
+               
+               
+           public void Item1(View view)
+             {
+               Button button2=findViewById(R.id.button2);
+               button2.setOnClickListener(new OnClickListener()
+                     {
+                         public void onClick(View view)
+                              {
+                                 
+                                   Toast.makeText(this,"Hey you Have done this!!",Toast.LENGTH_LONG).show();
+                                   
+                                   Intent intent=new Intent(this,YourNewActivity.class);
+                                   startActivity(intent);
+                                   finish();
+                              }
+                      });
+               }
+               
+               
+       
+       
+       
+       
+     }
